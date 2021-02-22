69ea3c8c32567badca475d3491fbafa2a85d5def
ahsan8244 <syed.ahsan8244@gmail.com>
2021-02-23 05:05:43 +0800

    use chrome aws lambda in prod

--- pages/api/gymTimeSlots.js
+++ pages/api/gymTimeSlots.js
@@ -1,25 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import puppeteer from "puppeteer"
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // running on the Vercel platform.
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  // running locally.
+  puppeteer = require("puppeteer");
+}
 
 export default async (req, res) => {
-  const browser = await puppeteer.launch();
+  const browser = process.env.AWS_LAMBDA_FUNCTION_VERSION
+    ? await chromium.puppeteer.launch({
+        args: ["--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      })
+    : await puppeteer.launch({
+        args: ["--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      });
   const page = await browser.newPage();
-  await page.goto("https://docs.google.com/forms/d/e/1FAIpQLSffxZ8i68wYTKzGI1d47koDxVTPu-Qg5S6TZJQn10QW4s9LFQ/viewform?gxids=7628");
+  await page.goto(
+    "https://docs.google.com/forms/d/e/1FAIpQLSffxZ8i68wYTKzGI1d47koDxVTPu-Qg5S6TZJQn10QW4s9LFQ/viewform?gxids=7628"
+  );
 
   let gyms = [
     {
       name: "CSE Active",
-      timeSlots: []
+      timeSlots: [],
     },
     {
       name: "Stanley Ho",
-      timeSlots: []
+      timeSlots: [],
     },
     {
       name: "B-Active",
-      timeSlots: []
-    }
-  ]
+      timeSlots: [],
+    },
+  ];
 
   await page.click("input[type=email]");
   await page.keyboard.type("hello@g.co");
@@ -32,51 +58,65 @@ export default async (req, res) => {
 
   for (let i = 0; i < gyms.length; i++) {
     await page.evaluate((i) => {
-      const radioButtons = document.getElementsByClassName("appsMaterialWizToggleRadiogroupOffRadio");
+      const radioButtons = document.getElementsByClassName(
+        "appsMaterialWizToggleRadiogroupOffRadio"
+      );
       Array.from(radioButtons)[i].click();
     }, i);
 
     await page.evaluate(() => {
-      const nextButton = Array.from(document.getElementsByClassName("appsMaterialWizButtonEl"))[0];
+      const nextButton = Array.from(
+        document.getElementsByClassName("appsMaterialWizButtonEl")
+      )[0];
       nextButton.click();
     });
 
-    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
 
     await page.evaluate(() => {
-      const radioButtons = document.getElementsByClassName("appsMaterialWizToggleRadiogroupOffRadio");
+      const radioButtons = document.getElementsByClassName(
+        "appsMaterialWizToggleRadiogroupOffRadio"
+      );
       Array.from(radioButtons)[0].click();
     });
 
     await page.evaluate(() => {
-      const nextButton = Array.from(document.getElementsByClassName("appsMaterialWizButtonEl"))[1];
+      const nextButton = Array.from(
+        document.getElementsByClassName("appsMaterialWizButtonEl")
+      )[1];
       nextButton.click();
     });
 
-    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
 
     const timeSlots = await page.evaluate(() => {
-      const slots = document.getElementsByClassName("quantumWizMenuPaperselectOption");
-      return Array.from(slots).map(slot => slot.textContent);
+      const slots = document.getElementsByClassName(
+        "quantumWizMenuPaperselectOption"
+      );
+      return Array.from(slots).map((slot) => slot.textContent);
     });
 
     gyms[i].timeSlots = timeSlots.slice(1);
-    
+
     await page.evaluate(() => {
-      const backButtton = Array.from(document.getElementsByClassName("appsMaterialWizButtonEl"))[0];
+      const backButtton = Array.from(
+        document.getElementsByClassName("appsMaterialWizButtonEl")
+      )[0];
       backButtton.click();
     });
 
-    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
 
     await page.evaluate(() => {
-      const backButtton = Array.from(document.getElementsByClassName("appsMaterialWizButtonEl"))[0];
+      const backButtton = Array.from(
+        document.getElementsByClassName("appsMaterialWizButtonEl")
+      )[0];
       backButtton.click();
     });
 
-    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
   }
 
   await browser.close();
   res.status(200).json(gyms);
-}
+};
